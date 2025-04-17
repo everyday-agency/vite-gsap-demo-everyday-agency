7954acb1d9fbaf7902d1b58fc72ce2dd9532ae4b
Benedikt Grether <info@benediktgrether.de>
2025-04-17 11:34:37 +0200

    Testing parallax scroll

--- src/components/gsapImageGalleryParallaxScroll.js
+++ src/components/gsapImageGalleryParallaxScroll.js
@@ -9,6 +9,8 @@ export function gsapImageGalleryParallaxScroll() {
         '[data-gsap-image-gallery]',
     );
 
+    const gsapHeadlineScale = document.querySelector('[data-headline-scale]');
+
     if (!gsapImageGalleryContainer) return;
 
     const timeline = gsap.timeline({
@@ -22,13 +24,25 @@ export function gsapImageGalleryParallaxScroll() {
         },
     });
 
-    timeline.to(gsapImageGalleryContainer.querySelectorAll('[data-speed]'), {
-        y: (i, el) =>
-            (1 - parseFloat(el.getAttribute('data-speed'))) *
-            gsapImageGalleryContainer.offsetHeight,
-        ease: 'none',
-        stagger: {
-            amount: 0.1,
-        },
-    });
+    timeline
+        .to(gsapImageGalleryContainer.querySelectorAll('[data-speed]'), {
+            y: (i, el) =>
+                (1 - parseFloat(el.getAttribute('data-speed'))) *
+                gsapImageGalleryContainer.offsetHeight,
+            ease: 'none',
+            stagger: {
+                amount: 0.1,
+            },
+        })
+        .to(gsapHeadlineScale, {
+            scrollTrigger: {
+                trigger: gsapHeadlineScale,
+                start: 'top bottom',
+                end: 'center center',
+                scrub: true,
+                markers: true,
+            },
+            scale: 2,
+            y: -200,
+        });
 }
